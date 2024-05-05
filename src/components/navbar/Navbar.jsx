1b059edb00767f955e263fe132cc412b33eb5322
Brachu <braian.ferrari.bf@gmail.com>
2024-05-04 21:11:06 -0300

    cambie algo del css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -28,6 +28,9 @@ function Navbar() {
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">Marcas</a>
                         </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#"></a>
+                        </li>
                     </ul>
                     <form className="d-flex" role="search">
                         <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar"/>
